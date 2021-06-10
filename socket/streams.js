0cbe4c510a1575dd5f0bceecfa3646ae40a06d92
Yonathan Martinez <33378318+MartinezYonathan@users.noreply.github.com>
2021-06-09 20:00:07 -0500

    Update streams.js

--- socket/streams.js
+++ socket/streams.js
@@ -2,6 +2,7 @@ module.exports = function (io, User, _) {
   const userData = new User();
   io.on('connection', (socket) => {
     socket.on('refresh', (data) => {
+      console.log("Entro " + data);
       io.emit('refreshPage', {});
     });
 
