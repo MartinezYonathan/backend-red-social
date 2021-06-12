5788c339347c174cec3557b805c4536e2db66326
MartinezYonathan <j-ma@hotmail.es>
2021-06-12 04:30:19 -0500

    se cambio cron */30

--- cron/cron.js
+++ cron/cron.js
@@ -1,6 +1,6 @@
 module.exports = function (io, cron) {
     console.log("run..");
-    cron.schedule('* * * * *', () => {
+    cron.schedule('*/30 * * * *', () => {
         console.log("En linia...");
         io.emit('serviceline',"En linia");
     });
